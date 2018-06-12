6556f395ea6ff5fdfb17f594da77a8ecf7716fee
SDevayani45 <36623877+SDevayani45@users.noreply.github.com>
2018-06-12 17:41:40 +0530

    Create swapop.c

---
+++ swapop.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int x,y;
+printf("enter two nos:");
+scanf("%d %d",&x,&y);
+printf("before swapping");
+printf("\nx=%d",x);
+printf("\ny=%d",y);
+x=x^y;
+y=x^y;
+x=x^y;
+printf("after swapping");
+printf("\nx=%d",x);
+printf("\ny=%d",y);
+getch();
+}
