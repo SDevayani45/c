8df8ca34a695a9672876717ba2f44aa228114e4b
SDevayani45 <36623877+SDevayani45@users.noreply.github.com>
2018-06-12 17:13:39 +0530

    Create numeric.c

---
+++ numeric.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+char st[50];
+int i,count=0;
+printf("enter the sentence");
+scanf("%[^\n]s",st);
+for(i=0;st[i]!='\0';i++)
+{
+if(st[i]>='0'&& st[i]<='9)
+{
+count+=1;
+}
+}
+printf("no of numerics:%d",count);
+getch();
+}
