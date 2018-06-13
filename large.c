908f7d03fd7c5aca5ebb9bca42e738c60750ca1c
SDevayani45 <36623877+SDevayani45@users.noreply.github.com>
2018-06-13 19:15:43 +0530

    Create large.c

---
+++ large.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+int a,b,c;
+printf("enter three nos:");
+scanf("%d %d %d",&a,&b,&c);
+if((a>b)&&(a>c))
+{
+printf("a is large");
+}
+else if(b>c)
+{
+printf("b is large");
+}
+else
+{
+printf("c is large");
+}
+getch();
+}
